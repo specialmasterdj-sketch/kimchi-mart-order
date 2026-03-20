26dd4cd37f0f4c5c2af57ad73c6b1397f207a60f
Dae Jung <specialmasterdj@gmail.com>
2026-03-20 10:29:28 -0400

    Initial commit: Kimchi Mart Auto Order System - 3 vendors, 1918 products

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'km-order-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './index.html',
+        './products.js',
+        './manifest.json'
+      ]);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response.ok && event.request.url.startsWith(self.location.origin)) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
